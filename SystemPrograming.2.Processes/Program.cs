cb171b83f72ffa0e649c6fdc31d563fd5553571a
СамигулинТ <@403_8_>
2018-02-21 15:13:49 +0600

    Add project files.

---
+++ SystemPrograming.2.Processes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemPrograming._2.Processes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
